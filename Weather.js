ac443c67c3f23d61a07351ce1ce7a16d13085ba7
jinwook-song <wlsdnr129@naver.com>
2021-06-14 18:12:05 +0900

    lecture 2.2 Icons and Styling

--- Weather.js
+++ Weather.js
@@ -1,11 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Weather({ temp }) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          name="weather-lightning-rainy"
+          size={96}
+          color="black"
+        />
+        <Text style={styles.temp}>{temp}º</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
     </View>
   );
 }
@@ -28,6 +37,14 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42,
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
